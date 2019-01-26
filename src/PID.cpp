e46dab0c0c9dd130e2ff6ef73ca0b78c914e1be3
Elias Khsheibun <elias3@gmail.com>
2019-01-26 11:21:53 +0100

    PID controller, README

--- src/PID.cpp
+++ src/PID.cpp
@@ -1,30 +1,26 @@
 #include "PID.h"
 
-/**
- * TODO: Complete the PID class. You may add any additional desired functions.
- */
-
 PID::PID() {}
 
 PID::~PID() {}
 
-void PID::Init(double Kp_, double Ki_, double Kd_) {
-  /**
-   * TODO: Initialize PID coefficients (and errors, if needed)
-   */
+void PID::Init(double Kp_, double Ki_, double Kd_)
+{
+  p_error = i_error = d_error = 0.0;
 
+  Kp = Kp_;
+  Ki = Ki_;
+  Kd = Kd_;
 }
 
-void PID::UpdateError(double cte) {
-  /**
-   * TODO: Update PID errors based on cte.
-   */
-
+void PID::UpdateError(double cte)
+{
+  i_error += cte;
+  d_error = cte - p_error;
+  p_error = cte;
 }
 
-double PID::TotalError() {
-  /**
-   * TODO: Calculate and return the total error
-   */
-  return 0.0;  // TODO: Add your total error calc here!
+double PID::TotalError()
+{
+  return Kp * p_error + Ki * i_error + Kd * d_error;
 }
